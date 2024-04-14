4cb0a289a59a48199af110c07d677bd0c6362172
Nandha Kumar Sivagnanam <59525732+nandha0150@users.noreply.github.com>
2024-04-15 04:19:21 +0530

    Competitive Programming
    Solving competitive programming to refresh DSA concepts

---
+++ 383. RansomNote.py
@@ -0,0 +1,44 @@
+# 383. Ransom Note
+# Solved
+# Easy
+# Topics
+# Companies
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+# Example 1:
+
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+# Example 2:
+
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+# Example 3:
+
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= ransomNote.length, magazine.length <= 105
+# ransomNote and magazine consist of lowercase English letters.
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        charcount = {}
+        for alph in magazine:
+            if alph in charcount:
+                charcount[alph] += 1
+            else:
+                charcount[alph] = 1
+        for chary in ransomNote:
+            if chary not in charcount or charcount[chary] == 0:
+                return False
+            else:
+                charcount[chary] -= 1
+        return True
